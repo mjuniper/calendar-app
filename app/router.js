cac817c4714c6b71c19fca1d47e5cef5e0f21f4f
mjuniper <mjuniper@gmail.com>
2018-01-17 14:31:30 -0700

    gh pages

---
+++ app/router.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('power-calendar');
+  this.route('my-calendar-component');
+});
+
+export default Router;
